7b7bbfd69cbb51986247b5a075d1a6a4b49295bf
ladybug-tentoumushi <126033194+ladybug-tentoumushi@users.noreply.github.com>
2023-03-10 13:53:44 +0900

    Update NextDayOfWeek.scala

--- src/main/scala/NextDayOfWeek.scala
+++ src/main/scala/NextDayOfWeek.scala
@@ -1,3 +1,12 @@
+sealed abstract class DayOfWeek
+case object Sunday extends DayOfWeek
+case object Monday extends DayOfWeek
+case object Tuesday extends DayOfWeek
+case object Wednesday extends DayOfWeek
+case object Thursday extends DayOfWeek
+case object Friday extends DayOfWeek
+case object Saturday extends DayOfWeek
+
 object NextDayOfWeek {
   def main(args: Array[String]): Unit = {
     val d = Friday
@@ -15,3 +24,5 @@ object NextDayOfWeek {
     }
   }
 }
+
+
