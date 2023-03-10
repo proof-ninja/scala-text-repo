8c98fb1bb4955e26008a698de7a4c0c93459fcf0
ladybug-tentoumushi <126033194+ladybug-tentoumushi@users.noreply.github.com>
2023-03-11 01:18:13 +0900

    Update Person.scala

--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -2,12 +2,9 @@ class Person (name : String, private val age : Int)
 
 object Person {
   def printPerson () : Unit = {
-    val taro = new Person("Taro",35)
+    val taro = new Person("Taro",30)
     println(taro.age)
   }
-}
-
-object Main {
   def main(args: Array[String]): Unit = {
     Person.printPerson()
   }
